fb835f4b1f1ea7fa7464f64b589c382befc9228a
trickbooter <paul@trickbooter.com>
2015-11-26 23:06:41 +1100

    simple node app

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM node:onbuild
+FROM node:5.1.0
 MAINTAINER Paul Harris <paul@trickbooter.com>
 
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app/
+RUN npm install
+COPY . /usr/src/app
+
 EXPOSE 3000
+
+CMD [ "node", "/usr/src/app/index.js" ]
+
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var server = require('./server');
-
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
+'use strict';
+var http = require('http');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3001, '0.0.0.0');
+console.log('server started');
+var signals = {
+  'SIGHUP': 1,
+  'SIGINT': 2,
+  'SIGTERM': 15
+};
+function shutdown(signal, value) {
+  server.close(function () {
+    console.log('server stopped by ' + signal);
+    process.exit(128 + value);
+  });
+}
+Object.keys(signals).forEach(function (signal) {
+  process.on(signal, function () {
+    shutdown(signal, signals[signal]);
+  });
 });
