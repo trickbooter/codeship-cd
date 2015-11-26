fe4fdf39ce95a55dea738ee6aaf4f98b4b54753a
trickbooter <paul@trickbooter.com>
2015-11-26 23:54:35 +1100

    adding mocha tests

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict';
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3001, '0.0.0.0');
+
+//var http = require('http');
+//var server = http.createServer(function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/plain'});
+//  res.end('Hello World\n');
+//}).listen(3001, '0.0.0.0');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = require('./server');
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
 console.log('server started');
+
+
 var signals = {
   'SIGHUP': 1,
   'SIGINT': 2,
